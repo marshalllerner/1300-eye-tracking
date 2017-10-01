7cce38fda6e8f15cc0356dea824dfcdd5daf74c1
Lily <lilymatcha@users.noreply.github.com>
2017-10-01 16:18:50 -0400

    initial commit

---
+++ stencil_heatmap.js
@@ -0,0 +1,46 @@
+const CSV_NAME = 'temp.csv';
+
+//ajax GET request to load the csv file and launch the heatmap
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: CSV_NAME,
+        dataType: "text",
+        success: function(data) {processData(data);}
+    });
+});
+
+//Actual process function
+function processData(allText) {
+  //Split csv lines by return \n and by commas
+  var allTextLines = allText.split(/\r\n|\n/);
+  var headers = allTextLines[0].split(',');
+  var datapoints = [];
+
+  //Here's where you need to write a loop that
+  //goes through all of csv, at each line, and
+  //pushes a heatmap data point onto the lines stack
+  //with the proper x and y values ()
+  //A heatmap data point looks like this object:
+  //{ x: ?, // x coordinate of the datapoint, a float 
+  //  y: ?, // y coordinate of the datapoint, a float
+  //  value: 50 // the value at datapoint(x, y) }
+  //"value" is arbitrary, keep at 50 but is adjustable for more/
+  //less intense maps
+
+  //Creates a heatmap instance
+  var heatmapInstance = h337.create({
+      container: document.querySelector('.heatmap')
+  });
+
+  //Initializes a heatmap data object with the lines
+  //stack datapoints
+  var data = {
+    max: lines.length,
+    min: 0,
+    data: datapoints
+  };
+
+  //Displays heatmap
+  heatmapInstance.setData(data);
+}
